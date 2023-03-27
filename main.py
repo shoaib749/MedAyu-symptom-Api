0cf185733472e8012e1398f485bbdbbd246d6c3a
Shoaib Ansari <11801@ceinsys.com>
2023-03-27 11:16:56 +0530

    tested for new input type

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ import nltk
 import operator
 from collections import Counter
 nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 
 
     
 app = Flask(__name__,template_folder='template')
+
+stop_words = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
+splitter = RegexpTokenizer(r'\w+')
 
 model = pickle.load(open("model.pkl",'rb'))
 ##use of common files and variables for predection & symptoms
@@ -154,7 +160,45 @@ def Enter():
     result = json.dumps({'result':found_symptoms})
     return result
 # returns the list of synonyms of the input word from thesaurus.com (https://www.thesaurus.com/) and wordnet (https://www.nltk.org/howto/wordnet.html)
+#testing new user_symptoms method
+def Enter():
+    Symptoms = str(request.form.get('user_symtoms')).lower.split(',')
+    print(Symptoms)
+    ##taking input is the thing after converting 
+    processed_user_symptoms=[]
+    for sym in user_symptoms:
+        sym=sym.strip()
+        sym=sym.replace('-',' ')
+        sym=sym.replace("'",'')
+        sym = ' '.join([lemmatizer.lemmatize(word) for word in splitter.tokenize(sym)])
+    processed_user_symptoms.append(sym)
 
+    user_symptoms = []
+    for user_sym in processed_user_symptoms:
+        user_sym = user_sym.split()
+        str_sym = set()
+        for comb in range(1, len(user_sym)+1):
+            for subset in combinations(user_sym, comb):
+                subset=' '.join(subset)
+                subset = synonyms(subset) 
+                str_sym.update(subset)
+        str_sym.add(' '.join(user_sym))
+        user_symptoms.append(' '.join(str_sym).replace('_',' '))
+    # Loop over all the symptoms in dataset and check its similarity score to the synonym string of the user-input 
+    # symptoms. If similarity>0.5, add the symptom to the final list
+    found_symptoms = set()
+    for idx, data_sym in enumerate(dataset_symptoms):
+        data_sym_split=data_sym.split()
+        for user_sym in user_symptoms:
+            count=0
+            for symp in data_sym_split:
+                if symp in user_sym.split():
+                    count+=1
+            if count/len(data_sym_split)>0.5:
+                found_symptoms.add(data_sym)
+    found_symptoms = list(found_symptoms)
+    result = json.dumps({'result':found_symptoms})
+    return result
 
 ##code starting for user selection me symptoms from the db
 @app.route('/db',methods=['POST'])
